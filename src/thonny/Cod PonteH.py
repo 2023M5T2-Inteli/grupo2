d597e723794aa429543c888e6faa8d27dfdc9108
IgorSFG <igor.garcia@sou.inteli.edu.br>
2023-03-23 14:33:13 -0300

    Código PonteH com Bomba d'água

--- src/thonny/Cod PonteH.py
+++ src/thonny/Cod PonteH.py
@@ -3,38 +3,37 @@ import machine
 import time
 
 # Definindo os pinos para a Ponte H
-pino1 = machine.Pin(0, machine.Pin.OUT)  # Pino 1 da Ponte H
-pino2 = machine.Pin(2, machine.Pin.OUT)  # Pino 3 da Ponte H
-pino3 = machine.Pin(7, machine.Pin.OUT)  # Pino 9 da Ponte H
-pino4 = machine.Pin(9, machine.Pin.OUT)  # Pino 10 da Ponte H
+bomb1 = machine.Pin(0, machine.Pin.OUT)  # Pino 1 da Ponte H
+bomb2 = machine.Pin(2, machine.Pin.OUT)  # Pino 3 da Ponte H
+ima1 = machine.Pin(7, machine.Pin.OUT)  # Pino 9 da Ponte H
+ima2 = machine.Pin(9, machine.Pin.OUT)  # Pino 10 da Ponte H
 
 # Função para ligar os imãs
 def ativaIma():
-    pino1.value(1)
-    pino2.value(0)
-    
-    pino3.value(1)
-    pino4.value(0)
+    ima1.value(1)
+    ima2.value(0)
     
     print("Ligado")
+    
+def ativaBomb():
+    bomb1.value(1)
+    bomb2.value(0)
+    
+def desativaBomb():
+    bomb1.value(0)
+    bomb2.value(0)
 
 # Função para inverter o campo magnético dos imãs
 def inverteIma():
-    pino1.value(0)
-    pino2.value(1)
-    
-    pino3.value(0)
-    pino4.value(1)
+    ima1.value(0)
+    ima2.value(1)
     
     print("Invertido")
 
 # Função para desligar os imãs
-def desligaIma():
-    pino1.value(0)
-    pino2.value(0)
-    
-    pino3.value(0)
-    pino4.value(0)
+def desativaIma():
+    ima1.value(0)
+    ima2.value(0)
     
     print("Desligado")
 
@@ -44,13 +43,19 @@ while True:
     
     ativaIma()
     
-    time.sleep(54) # Tempo do imã ligado 
+    time.sleep(27) # Tempo do imã ligado
+    
+    ativaBomb()
+    
+    time.sleep(27) # Tempo do imã ligado
+    
+    desativaBomb()
     
     inverteIma()
     
     time.sleep(1) # Tempo da inversão do campo do imã
     
-    desligaIma()
+    desativaIma()
     
     time.sleep(11) # Tempo do imã desligado
     
