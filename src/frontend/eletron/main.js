9d65df736c36f334cac5304b05f3a668778294b5
Israel <israel.carvalho@sou.inteli.edu.br>
2023-03-25 13:14:32 -0300

    Merge dev with apresentacao-s4

--- src/frontend/eletron/main.js
+++ src/frontend/eletron/main.js
@@ -1,6 +1,5 @@
 const { app, BrowserWindow } = require("electron");
 // const path = require("path");
-const { spawn } = require("child_process");
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -11,6 +10,17 @@ const createWindow = () => {
     },
   });
   // launch flask server
+  const { spawn } = require("child_process");
+  const pythonProcess = spawn("python", ["./main.py"]);
+  pythonProcess.stdout.on("data", (data) => {
+    console.log(data.toString());
+  });
+  pythonProcess.stderr.on("data", (data) => {
+    console.log(data.toString());
+  });
+  pythonProcess.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
 
   win.loadFile("index.html");
 };
@@ -19,73 +29,8 @@ app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
 });
 
-const py_installLibs = () => {
-  return new Promise((resolve, reject) => {
-    const pythonProcess = spawn("python", ["./startup/install_libs.py"]);
-
-    pythonProcess.stdout.on("data", (data) => {
-      console.log(data.toString());
-    });
-    pythonProcess.stderr.on("data", (data) => {
-      console.log(data.toString());
-    });
-    pythonProcess.on("close", (code) => {
-      console.log(`child process exited with code ${code}`);
-      resolve();
-    });
-  });
-};
-const py_connectDobot = () => {
-  return new Promise((resolve, reject) => {
-    const pythonProcess = spawn("python", ["./startup/connect_dobot.py"]);
-
-    pythonProcess.stdout.on("data", (data) => {
-      console.log(data.toString());
-      if (data.includes("PORT==")) {
-        let port = data.toString();
-        port = port.replace("PORT==", "");
-        port = port.replace("==", "");
-        console.log("A PORTA DO DOBOT É", Number(port));
-        resolve(Number(port));
-      }
-    });
-    pythonProcess.on("close", (code) => {
-      console.log(`child process exited with code ${code}`);
-    });
-  });
-};
-const py_runServer = (dobotPort) => {
-  return new Promise((resolve, reject) => {
-    const pythonProcess = spawn("python", [
-      "./backend_flask/app.py",
-      dobotPort,
-    ]);
-
-    pythonProcess.stdout.on("data", (data) => {
-      console.log(data.toString());
-    });
-    pythonProcess.stderr.on("data", (data) => {
-      console.log(data.toString());
-    });
-    pythonProcess.on("close", (code) => {
-      console.log(`child process exited with code ${code}`);
-      resolve();
-    });
-  });
-};
-
-app.whenReady().then(async () => {
-  console.log("--------------INSTALANDO LIBS NECESSARIAS--------------");
-  await py_installLibs();
-
-  console.log("--------------EXECUTANDO SCRIPT PYTHON-----------------");
-  dobotPort = await py_connectDobot();
-
-  console.log("--------------INICIANDO SERVIDOR-----------------------");
-  py_runServer(dobotPort);
-  setTimeout(()=>{
-    createWindow();
-  }, 5000)
+app.whenReady().then(() => {
+  createWindow();
 });
 
 // const create_window2 = () => {
