3e476c46824843f047c239d41fa7f385dd6a20a2
IgorSFG <igor.garcia@sou.inteli.edu.br>
2023-04-04 16:29:27 -0300

    feat(embedded): added control.py

---
+++ src/embedded/control.py
@@ -0,0 +1,35 @@
+import serial.tools.list_ports
+from time import sleep
+
+timewait = 2
+baudrate = 115200
+
+# Lista todas as portas COM, uma delas é o Raspberry Pi Pico
+def find_coms():
+
+    ports_found = serial.tools.list_ports.comports()
+    print("[COM ports found]")
+    for item in ports_found:
+        print(str(item))
+
+    return ports_found
+
+find_coms() # Executa a função
+
+ser = serial.Serial("COM27", baudrate, timeout = timewait) # Abre um objeto de comunicação com a porta na qual o Raspberry Pi Pico esta
+
+while True:
+    print("base")
+    ser.write(b"on\n")
+    line = ser.readline() # Realiza a leitura de um pacote de informaçãoes via serial
+    print(line.decode("UTF-8")) # Exibe a string que foi enviada pelo Raspberry Pi Pico decodificada como texto
+    sleep(2)
+
+    line = ser.readline()
+    print(line.decode("UTF-8"))
+
+    print("medindo")
+    ser.write(b"off\n")
+    line = ser.readline() # Realiza a leitura de um pacote de informaçãoes via serial
+    print(line.decode("UTF-8")) # Exibe a string que foi enviada pelo Raspberry Pi Pico decodificada como texto
+    sleep(2)
