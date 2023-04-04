39004a06991988c745896859c50de53269c6fd5a
IgorSFG <igor.garcia@sou.inteli.edu.br>
2023-04-04 16:05:49 -0300

    Célula de Carga

--- src/thonny/Cod hx711.py
+++ src/thonny/Cod hx711.py
@@ -1,4 +1,5 @@
 import machine
+import sys
 from hx711 import HX711
 from time import sleep
 
@@ -6,22 +7,27 @@ from time import sleep
 hx = HX711(2, 3)
 # valor base de medida
 base = 142000
-
+    
 #Função para ler e retornar os valores da célula de carga
 def readCell(load_cell):
     val = load_cell.read()
     print("Load cell value: ", val)
     return val
 
-def convert(vl):
-    wt = (vl - base)/1070
+def convert(vl, bs):
+    wt = (vl - bs)/1655
     print("Weight: ", wt)
     return wt
     
 # Lendo o valor da célula de carga e printando
 while True:
+    base = readCell(hx)
+    sys.stdout.write("Medindo...\n")
+    sleep(5)
     value = readCell(hx)
-    weight = convert(value)
+    weight = convert(value, base)
+    sys.stdout.write(str(weight))
+    print("\n")
     sleep(2)
     
     
