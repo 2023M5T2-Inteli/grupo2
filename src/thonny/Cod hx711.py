62b347590bbae916cba7339760a9a937b95ec79b
Caio <Caio.abreu@sou.inteli.edu.br>
2023-04-04 16:34:06 -0300

    Revert "Célula de Carga"
    This reverts commit 39004a06991988c745896859c50de53269c6fd5a.

--- src/thonny/Cod hx711.py
+++ src/thonny/Cod hx711.py
@@ -1,5 +1,4 @@
 import machine
-import sys
 from hx711 import HX711
 from time import sleep
 
@@ -7,27 +6,22 @@ from time import sleep
 hx = HX711(2, 3)
 # valor base de medida
 base = 142000
-    
+
 #Função para ler e retornar os valores da célula de carga
 def readCell(load_cell):
     val = load_cell.read()
     print("Load cell value: ", val)
     return val
 
-def convert(vl, bs):
-    wt = (vl - bs)/1655
+def convert(vl):
+    wt = (vl - base)/1070
     print("Weight: ", wt)
     return wt
     
 # Lendo o valor da célula de carga e printando
 while True:
-    base = readCell(hx)
-    sys.stdout.write("Medindo...\n")
-    sleep(5)
     value = readCell(hx)
-    weight = convert(value, base)
-    sys.stdout.write(str(weight))
-    print("\n")
+    weight = convert(value)
     sleep(2)
     
     
