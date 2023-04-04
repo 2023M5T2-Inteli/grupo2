c4418e32f994f379d1bc3e14f32d3701506740e5
Israel <israel.carvalho@sou.inteli.edu.br>
2023-04-04 12:03:23 -0300

    Auto stash before rebase of "origin/dev"

--- src/app/api/app.py
+++ src/app/api/app.py
@@ -4,7 +4,7 @@ from extensions import db
 
 #initialize the app
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite+pysqlite:///project.db"
 
 
 #initialize the database
--- src/app/scripts/ensaios.js
+++ src/app/scripts/ensaios.js
@@ -1,3 +1,6 @@
+var track = [[1,true],[2,false]];
+var order = 0;
+
 // body onload get all routes from database
 getRoutes = () => {
   console.log("getRoutes");
@@ -34,22 +37,90 @@ getRoutes = () => {
 };
 
 // Função que altera o botão de ligar/desligar imã e envia o comando para o raspberry
-function magnetonoff() {
-  const magnetButton = document.getElementById("magnetButton");
-  if (magnetButton.innerHTML == "Ligar Imã") {
-    magnetButton.innerHTML = "Desligar Imã";
-    axios.get("http://localhost:5000/magnet_on").then(function (response) {
-      console.log(response.data);
-    });
-  } else {
-    magnetButton.innerHTML = "Ligar Imã";
-    axios.get("http://localhost:5000/magnet_off").then(function (response) {
-      console.log(response.data);
-    });
+// function magnetonoff() {
+//   const magnetButton = document.getElementById("magnetButton");
+//   if (magnetButton.innerHTML == "Ligar Imã") {
+//     magnetButton.innerHTML = "Desligar Imã";
+//     axios.get("http://localhost:5000/magnet_on").then(function (response) {
+//       console.log(response.data);
+//     });
+//   } else {
+//     magnetButton.innerHTML = "Ligar Imã";
+//     axios.get("http://localhost:5000/magnet_off").then(function (response) {
+//       console.log(response.data);
+//     });
+//   }
+// }
+
+function magnetRecord(){
+  const magnetButtonRecord = document.getElementById("magnetButtonRecord");
+  if (magnetButtonRecord.innerHTML == "Imã Ligado") {
+    magnetButtonRecord.innerHTML = "Imã Desligado";
+    magnetButtonRecord.classList.remove("greenbg");
+    magnetButtonRecord.classList.add("redbg");
+  }
+  else{
+    magnetButtonRecord.innerHTML = "Imã Ligado";
+    magnetButtonRecord.classList.remove("redbg");
+    magnetButtonRecord.classList.add("greenbg");
   }
 }
 
-async function makePostRequest(test) {
+// Função que retorna o status do imã
+function magnetStatus(){
+  const magnetButtonRecord = document.getElementById("magnetButtonRecord");
+  if (magnetButtonRecord.innerHTML == "Imã Ligado") {
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+// Pega a posição atual do robô e insere no array track
+function addNodePost(){
+  axios
+    .get("http://localhost:5000/add_position_dobot")
+    .then(function (response) {
+      // apeend into track response and order
+      track.push(response.x, response.y, response.z, response.r, order, magnetStatus());
+      order++;
+      console.log(track);
+    }
+    )
+    .catch(function (error) {
+      console.log(error);
+    }
+    );
+}
+
+function mkArray(){
+  for(let i = 0; i < track.length; i++){
+    // add name in track for each position
+    const name = document.getElementById("trackName").value;
+    track[i].push(name);
+  }
+  console.log(track);
+  let trackPost = {"data": track}
+  order = 0
+  track = []
+  // make trackPost a json with the "data:" object
+  return trackPost
+}
+
+function sendPostRoute(){
+  axios
+    .post("http://localhost:5000/add_track", mkArray())
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+
+async function postRotaPadrao(test) {
   axios
     .post("http://localhost:5000/robot", test)
     .then(function (response) {
@@ -59,3 +130,5 @@ async function makePostRequest(test) {
       console.log(error);
     });
 }
+
+// {"data": [array]}
